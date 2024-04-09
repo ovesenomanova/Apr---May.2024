f808af5ea24cb4a7ac55c11ade8cf4cda908018c
EOV <sweet16102019@gmail.com>
2024-04-09 18:49:01 +0400

    ООП ССоздайте класс «Дробь». Необходимо хранить в полях класса: числитель и знаменатель. Реализуйтеметодыкласса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса. Также создайте методы класса для выполнения арифметических опера ций (сложение, вычитание, умножение, делени

--- 04.04.2024_4.py
+++ 04.04.2024_4.py
@@ -27,4 +27,12 @@ class Fraction:
         n = self.n * other.d
         d = self.d * other.n
         return Fraction(n, d)
+    def __repr__(self):
+        return f'{self.n}/{self.d}'
 
+
+i1 = Fraction(3, 5)
+i2 = Fraction(4, 9)
+print(i1 + i2, i1 - i2,)
+print( i1 * i2)
+print(i1/i2)
