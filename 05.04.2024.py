9e2deebe4dcf319db495ca26742249a47f8bfb2b
EOV <sweet16102019@gmail.com>
2024-04-09 21:10:29 +0400

    Напишите класс Вектора. У него должно быть реализовано два свойства: x,y - реализуйте возможность узнать длину через функцию len (Vector) - возможность умножить вектор на число. Его координаты умножаются  - возможность сложить вектор с другим вектором "Х" складывается с "Х", "Y" с "Y"  - написать метод normalize, который должен нормализовать вектор. Нормализовать - поделить координаторы вектора на длину это вектора.

--- 05.04.2024.py
+++ 05.04.2024.py
@@ -1,14 +1,24 @@
 class Human:
+
     def __init__(self, name, age, height):
         self.name = name
         self.age = age
         self.height = height
+
     def __len__(self):
         return self.height
+
     def __add__(self, other):
         return Human(self.name, self.age + other, self.height)
+
     def __repr__(self):
         return f'HUMAN {self.name}'
+
+    def get_x(self):
+        return self.name
+
+    def set_x(self, value):
+        self.name = value
 
 
 h = Human('Petre', 20, 180)
