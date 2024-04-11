13300b2a30fc63d9ad8a1d4182427b179f6aca07
EOV <sweet16102019@gmail.com>
2024-04-11 21:19:01 +0400

    Напишите класс Вектора. У него должно быть реализовано два свойства: x, y - реализуйте возможность узнать длину через функцию len (Vector) - возможность умножить вектор на число. Его координаты умножаются  - возможность сложить вектор с другим вектором "Х" складывается с "Х", "Y" с "Y"  - написать метод normalize, который должен нормализовать вектор. Нормализовать - поделить координаторы вектора на длину это вектора.  - напишите функцию (не метод), которая принимает два вектора и выдает расстояние между ними.  -напишите функцию min_len(vecs), которая принимает список векторов и возвращает вектор с минимальной длиной

--- 05.04.2024.py
+++ 05.04.2024.py
@@ -1,9 +1,15 @@
 class Human:
+    counter = 0
+
+    @staticmethod
+    def total_amount():
+        return Human.counter
 
     def __init__(self, name, age, height):
         self.name = name
         self.age = age
         self.height = height
+        Human.counter += 1
 
     def __len__(self):
         return self.height
@@ -22,9 +28,13 @@ class Human:
 
 
 h = Human('Petre', 20, 180)
+h1 = Human('Jon', 15, 170)
+h2 = Human('Olga', 17, 160)
 print( len(h) )
 h = h + 5
 print(h.age, h.name)
+print(Human.total_amount())
+
 
 
 
