faab6e698ed47075041dcba7159a2aefaae9ac93
ovesenomanova <165815707+ovesenomanova@users.noreply.github.com>
2024-04-18 14:23:28 +0400

    Add files via upload

---
+++ 14.04.2024.py
@@ -0,0 +1,57 @@
+class Auto:
+                                            #     Реализуйте класс «Автомобиль». Необходимо хранить
+                                            # в полях класса: название модели, год выпуска, производителя, объем двигателя, цвет машины, цену.
+    def __unit__(self, model, year, brand, capacity, color, price):
+        self.model = model
+        self.year = year
+        self.brand = brand
+        self.capacity = capacity
+        self.color = color
+        self.price = price
+                                                # Реализуйте методы класса для ввода данных, вывода данных
+    def print_(self):
+        print_(
+            f'Модель{self.model}, марка автомобиля {self.brand} {self.year} год выпуска с объемом двигателя {self.capacity} литров, цвет {self.color} и стоимостью {self.price} рублей')
+
+    def input_(self):
+        input_(f' Заполните поля для автомобиля. '
+               f'Модель{self.model}, марка автомобиля {self.brand} {self.year} год выпуска с объемом двигателя {self.capacity} литров, цвет {self.color} и стоимостью {self.price} рублей ')
+                                                # Реализуйте доступ к отдельным полям через методы класса
+    def set_model(self, model_new):
+        self.model = model_new
+
+    def set_year(self, year_new):
+        self.year = year_new
+
+    def set_brand(self, brand_new):
+        self.brand = brand_new
+
+    def set_capacity(self, capacity_new):
+        self.capacity = capacity_new
+
+    def set_color(self, color_new):
+        self.color = color_new
+
+    def set_price(self, price_new):
+        self.price = price_new
+
+    def get_model(self):
+        return self.model
+
+    def get_year(self):
+        return self.year
+
+    def get_brand(self):
+        return self.brand
+
+    def get_capacity(self):
+        return self.capacity
+
+    def get_color(self):
+        return self.color
+
+    def get_price(self):
+        return self.price
+
+    def __repr__(self):
+        return f'{self.model, self.year, self.brand, self.capacity, self.color, self.price}'
