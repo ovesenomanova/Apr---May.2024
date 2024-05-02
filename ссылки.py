396ec80444ab742346758eee41ca38314f34861f
ovesenomanova <165815707+ovesenomanova@users.noreply.github.com>
2024-05-02 20:57:36 +0400

    Add files via upload

--- ссылки.py
+++ ссылки.py
@@ -1,1 +1,7 @@
-https://nagornyy.me/it/obektno-orientirovannoe-programmirovanie-v-python/
+
+https://cmp.phys.msu.su/sites/default/files/%D0%9E%D0%9E%D0%9F_%D0%BD%D0%B0_Python_%D0%A3%D1%87%D0%B5%D0%B1%D0%BD%D0%BE%D0%B5%20%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%B8%D0%B5_var7.pdf
+https://cpp-python-nsu.inp.nsk.su/textbook/sec4/ch4
+https://www.youtube.com/watch?v=bClOG0X6bWo
+
+https://www.youtube.com/@ITVDN/search?query=Python
+
