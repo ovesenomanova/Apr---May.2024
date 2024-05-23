936c9ebe2a679ad3ace3e4a58dab18fa0f721d1a
ovesenomanova <165815707+ovesenomanova@users.noreply.github.com>
2024-05-23 21:39:12 +0400

    Add files via upload

---
+++ 23.05.2024_2.py
@@ -0,0 +1,48 @@
+import sqlite3
+conn = sqlite3.connect('C:\\Users\\Stud\\Desktop\\eov\\23.05.2024_2.db')
+check_same_thread = False
+cursor = sqlite3.Cursor(conn)
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    login VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL
+)""")
+cursor.execute(
+    """
+      CREATE TABLE IF NOT EXISTS orders (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      users_id INTEGER,
+      
+      FOREIGN KEY (users_id) REFERENCES users (id)
+      ON DELETE CASCADE
+      )
+    """)
+cursor.execute(
+    """
+      CREATE TABLE IF NOT EXISTS meals (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name VARCHAR(20))
+    """)
+cursor.execute(
+    """
+      CREATE TABLE IF NOT EXISTS orders_meal (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      order_id INTEGER,
+      meal_id INTEGER,
+      
+      FOREIGN KEY (order_id) REFERENCES orders (id)
+      ON DELETE CASCADE,
+      FOREIGN KEY (meal_id) REFERENCES meals (id)
+      ON DELETE CASCADE
+    )""")
+
+cursor.execute('INSERT INTO meals (name) VALUES ("манты")')
+meal_id = cursor.lastrowid
+cursor.execute('INSERT INTO orders (id) VALUES (2)')
+order_id = cursor.lastrowid
+
+cursor.execute(f'INSERT INTO orders_meal (order_id, meal_id) VALUES ({order_id}, {meal_id})')
+conn.commit()
+
